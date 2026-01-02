b4e623d12796def54c4faaa54d4513ba593f1dcd
maisonhenri69-source <maisonhenri69@gmail.com>
2026-01-02 14:12:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // Cette ligne est cruciale pour que le site fonctionne une fois mis en ligne
   // peu importe où il est hébergé (sous-dossier ou racine)
-  base: './', 
+  base: '/maisonhenri/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
